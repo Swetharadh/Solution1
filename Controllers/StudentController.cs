3180c1b0625c3ea52b7ab61847cf46ecd12ea927
swetha.radhakrish <swetha.radhakrish@ASPLAPR406>
2021-09-06 09:28:49 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,63 @@
+﻿using CRUDAPI.Data;
+using CRUDAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+namespace CRUDAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        private readonly Context _context;
+        public StudentController(Context context)
+        {
+            _context = context;
+        }
+        //Get all the students
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return _context.student.ToList();
+        }
+        [HttpGet("{Id}")]
+        public Student GetStudent(int id)
+        {
+            var per = _context.student.Where(i => i.Id == id).FirstOrDefault();
+            return per;
+        }
+        [HttpPost]
+        public ActionResult InsertStudent([FromBody] Student student)
+        {
+            _context.student.Add(student);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.student.FindAsync(id);
+            if (student == null)
+            {
+                return NoContent();
+            }
+            _context.student.Remove(student);
+            await _context.SaveChangesAsync();
+            return NotFound();
+        }
+        [HttpPut]
+        public ActionResult UpdateStudent([FromBody] Student s)
+        {
+            var per = _context.student.Where(i => i.Id == s.Id).FirstOrDefault();
+            per.ProductName = s.ProductName;
+            per.Price = s.Price;
+            per.Quantity = s.Quantity;
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
+
+
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAPI.Models
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
